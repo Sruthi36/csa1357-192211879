1b8ca58dbba48878783699490f468aa1e714da1f
Sruthi36 <124115664+Sruthi36@users.noreply.github.com>
2024-07-31 14:01:21 +0530

    Update 4CFG.cpp

--- 4CFG.cpp
+++ 4CFG.cpp
@@ -1,45 +1,26 @@
 #include<stdio.h>
 #include<string.h>
-main()
+#include<math.h>
+int main()
 {
-char s[100];
-int i,flag,flag1,a,b;
-int l;
-printf("enter a string to check:");
-scanf("%s",s);
-l=strlen(s);
-flag=1;
-for(i=0;i<l;i++)
-{
-if(s[i]!='0' && s[i]!='1')
-{
-flag=0;
-}
-}
-if(flag!=1)
-printf("string is Not Valid\n");
-if(flag==1)
-{
-flag1=1;
-a=0;b=l-1;
-while(a!=(l/2))
-{
-if(s[a]!=s[b])
-{
-flag1=0;
-}
-a=a+1;
-b=b-1;
-}
-if (flag1==1)
-{
- printf("The string is a palindrome\n");
- printf("string is accepted\n");
-}
-else
-{
- printf("The string is not a palindrome\n");
- printf("string is Not accepted\n");
- }
-}
+	char s[100];
+	int l,i=0,a,b,flag=0;
+	printf("enter a string to check:");
+	scanf("%s",s);
+	l=strlen(s);
+	for (int i=0;i<(floor(l/2));i++){
+		if (s[i] == s[l-1-i]){
+			flag = 0;
+		}
+		else{
+			flag = 1;
+		}
+	}
+	if (flag == 0){
+		printf("The string is accepted \nGiven String is Palindrome");
+	}
+	else{
+		printf("The string is not accepted \nGiven String is Not a Palindrome");
+	}
+	return 0;
 }
