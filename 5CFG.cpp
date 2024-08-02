518786d405cb7310c5a5803a0e185eb3721b36a7
Sruthi36 <124115664+Sruthi36@users.noreply.github.com>
2024-08-02 13:32:47 +0530

    Update 5CFG.cpp

--- 5CFG.cpp
+++ 5CFG.cpp
@@ -1,66 +1,25 @@
 #include<stdio.h>
 #include<string.h>
-main()
-{
-char s[100];
-int i,flag,flag1,a,b;
-int l,count1,count2;
-printf("enter a string to check:");
-scanf("%s",s);
-l=strlen(s);
-flag=1;
-for(i=0;i<l;i++)
-{
-if(s[i]!='0' && s[i]!='1')
-{
- flag=0;
-}
-}
-if(flag!=1)
-printf("string is Not Valid\n");
-if(flag==1)
-{
-i=0;count1=0;
-while(s[i]=='0') 
-{
-count1++;
-i++;
-}
-while(s[i]=='1')
-{
-i++; 
-}
-flag1=1;
-count2=0;
-while(i<l)
-{
-if(s[i]=='0')
-{
-count2++;
-}
-else
-{
-flag1=0;
-}
-i++;
-}
-if(flag1==1)
-{
-if(count1==count2)
- {
- printf("The string satisfies the condition 0n1m0n\n");
- printf("String Accepted\n");
- }
- else
- {
- printf("The string does not satisfy the condition 0n1m0n\n");
- printf("String Not Accepted\n");
- }
-}
-else
- {
- printf("The string does not satisfy the condition 0n1m0n\n");
- printf("String Not Accepted\n");
-}
-}
+int main(){
+	char s[100];
+	int i,l,flag=0;
+	printf("Enter a string: ");
+	scanf("%s",&s);
+	l = strlen(s);
+	printf("%c",s[0]);
+	if (s[0] == '1' || s[l-1] == '1'){
+		flag = 1;
+	}
+	for (i=1;i<l-1;i++){
+		if (s[i] == '0'){
+			flag = 1;
+		}
+	}
+	if (flag == 0){
+		printf("Given string is accepted.");
+	}
+	else{
+		printf("Given string is not accepted.");
+	}
+	return 0;
 }
